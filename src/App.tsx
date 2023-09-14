516e1f6187c4baf6a19ad441e606128d5b3e0d40
JKyEun <j56237@naver.com>
2023-09-14 17:36:03 +0900

    feat: 페이지 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import SignInPage from './pages/SignInPage';
+import MainPage from './pages/MainPage';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/login" element={<SignInPage />} />
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
