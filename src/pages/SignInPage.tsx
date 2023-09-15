07417235e6622547da7b593c7bea6891e23fe522
JKyEun <j56237@naver.com>
2023-09-15 13:52:29 +0900

    feat: 로그인 페이지 퍼블리싱

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,5 +1,28 @@
 import React from 'react';
+import '../style/signInPage.scss';
 
 export default function SignInPage() {
-  return <div>SignInPage</div>;
+  return (
+    <div className="signin-page">
+      <div className="img-wrap">
+        <img src="/gitFollower-logo.png" alt="깃팔로워 로고" />
+      </div>
+      <div className="login-wrap">
+        <div className="nickname">
+          <div>Github Nickname</div>
+          <input type="text" />
+          <button type="button">Check</button>
+        </div>
+        <div className="token">
+          <div>Github Token</div>
+          <input type="text" />
+          <button type="button">Check</button>
+        </div>
+        <div className="register">
+          <button type="submit">Register</button>
+          <div>깃허브에서 찾을 수 없는 닉네임입니다.</div>
+        </div>
+      </div>
+    </div>
+  );
 }
